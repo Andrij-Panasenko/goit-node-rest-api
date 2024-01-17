034d31c9292fdbc1f2abe06c94ed8bb454f264b2
a.panasenko <andrii.panasenko295@gmail.com>
2024-01-17 20:33:59 +0200

    prep

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,21 @@
+import contactsService from "../services/contactsServices.js";
+
+export const getAllContacts = (req, res) => {
+   const {url, method} = req;
+};
+
+export const getContactById = (req, res) => {
+   const {url, method} = req;
+};
+
+export const deleteContact = (req, res) => {
+   const {url, method} = req;
+};
+
+export const createContact = (req, res) => {
+   const {url, method} = req;
+};
+
+export const updateContact = (req, res) => {
+   const {url, method} = req;
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,66 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const listContact = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getContactById = async (id) => {
+  try {
+    const contacts = await listContact();
+    const result = contacts.find((item) => item.id === id);
+    return result || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const addContact = async ({ name, email, phone }) => {
+  try {
+    const contacts = await listContact();
+    const newContact = {
+      id: nanoid(),
+      name,
+      phone,
+      email,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const removeContact = async (id) => {
+  try {
+    const contacts = await listContact();
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index === -1) return null;
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+  listContact,
+  getContactById,
+  removeContact,
+  addContact,
+};
+// fs.readFile(filename, [options]) - читання файлу
+// fs.writeFile(filename, data, [options]) - запис файлу
+// fs.appendFile(filename, data, [options]) - додавання у файл
+// fs.rename(oldPath, newPath) - перейменування файлу.
+// fs.unlink(path, callback) - видалення файлу.
