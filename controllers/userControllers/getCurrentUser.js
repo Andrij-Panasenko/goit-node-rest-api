21e3fd9e092128f781d46a8b3c7655b07b6f94dc
a.panasenko <andrii.panasenko295@gmail.com>
2024-02-03 00:46:00 +0200

    getCurrent

--- controllers/userControllers/getCurrentUser.js
+++ controllers/userControllers/getCurrentUser.js
@@ -1,5 +1,16 @@
-const getCurrentUser = () => {
-    
-}
+const User = require("../../models/users");
+const controllerWrapper = require("../../helpers/controllerWraper");
+const HttpError = require("../../helpers/HttpError");
 
-module.exports = getCurrentUser;
+const getCurrentUser = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  const [_, token] = authorization.split(" ");
+
+  const user = await User.findOne({ token }).select("-password");
+  if (!user) throw HttpError(401);
+
+  res.status(200).json(user);
+};
+
+module.exports = controllerWrapper(getCurrentUser);
--- controllers/userControllers/logoutUser.js
+++ controllers/userControllers/logoutUser.js
@@ -1,19 +1,20 @@
 const HttpError = require("../../helpers/HttpError");
+const controllerWrapper = require("../../helpers/controllerWraper");
 const User = require("../../models/users");
 
 const logoutUser = async (req, res, next) => {
   const { id } = req.body;
 
-  try {
+  
     const user = await User.findById(id);
     if (!user || user.token === "") next(HttpError(401));
 
     await User.findByIdAndUpdate(user._id, { token: "" });
 
     res.status(204).json();
-  } catch (error) {
+  
     next(HttpError(401));
-  }
+
 };
 
-module.exports = logoutUser;
+module.exports = controllerWrapper(logoutUser);
