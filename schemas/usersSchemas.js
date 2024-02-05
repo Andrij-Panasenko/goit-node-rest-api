953d3f84d5128f65c5be93af6f8464395276c925
a.panasenko <andrii.panasenko295@gmail.com>
2024-02-05 18:43:56 +0200

    last fix

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,7 +1,6 @@
 const joi = require("joi");
 
 const registerSchema = joi.object({
-  name: joi.string().min(2).max(15).required(),
   email: joi.string().email().max(50).required(),
   password: joi.string().min(6).max(18).required(),
 });
