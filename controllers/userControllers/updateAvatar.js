eefb5b00769dc38aea944b651e4e37af4bc86ce8
a.panasenko <andrii.panasenko295@gmail.com>
2024-02-07 18:58:11 +0200

    first fersion updateAvatar

--- controllers/userControllers/updateAvatar.js
+++ controllers/userControllers/updateAvatar.js
@@ -1,5 +1,15 @@
 const controllerWrapper = require("../../helpers/controllerWraper");
+const path = require('path');
+const fs = require('fs/promises');
 
-const updateAvatar = () => { };
+const avatarsDir = path.join(__dirname, "avatars");
+
+const updateAvatar = async (req, res, next) => {
+    const { path: tempPath, filename } = req.file;
+    const resultPath = path.join(avatarsDir, filename);
+    await fs.rename(tempPath, resultPath);
+
+    res.json({ok:true})
+ };
 
 module.exports = controllerWrapper(updateAvatar);
--- middlewears/multerConfig.js
+++ middlewears/multerConfig.js
@@ -2,9 +2,9 @@ const multer = require("multer");
 const path = require("path");
 
 const multerConfig = multer.diskStorage({
-  destination: path.join(__dirname, "tmp"),
+  destination: path.join(__dirname, "../", "tmp"),
   filename: (req, file, cb) => {
-    cb(null, Date.now() + file.originalName);
+    cb(null, Date.now() + "_" + file.originalname);
   },
 });
 
