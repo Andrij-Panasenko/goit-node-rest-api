f978ba6a27ab9504837c09bc48c036cbb2e82667
a.panasenko <andrii.panasenko295@gmail.com>
2024-02-07 17:19:19 +0200

    add avatarURL generation

--- controllers/userControllers/registerUser.js
+++ controllers/userControllers/registerUser.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const Users = require("../../models/users");
 const HttpError = require("../../helpers/HttpError");
+const gravatar = require('gravatar');
 
 
 const registerUser = async (req, res, next) => {
@@ -8,8 +9,10 @@ const registerUser = async (req, res, next) => {
   const hashedPsw = await bcrypt.hash(password, 10);
   
   try {
-    const result = await Users.create({ email, password: hashedPsw });
-    res.status(201).json({id: result._id, email});
+    const avatar = gravatar.url(email, {s: "200"})
+    const result = await Users.create({ email, password: hashedPsw, avatar });
+    console.log(result.avatar)
+    res.status(201).json({id: result._id, email, avatarURL: avatar});
   } catch (error) {
     if (error.message.includes("E11000")) {
       next(HttpError(409, "Not valid email"));
